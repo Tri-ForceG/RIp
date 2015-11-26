733c18dc23d860b7e74b80d8cdce8a48bbeb889a
Tri-ForceG <Jonhcenarules@hotmail.com>
2015-11-26 10:28:52 -0800

    Links

--- core/src/com/mygdx/game/gravtest.java
+++ core/src/com/mygdx/game/gravtest.java
@@ -281,3 +281,4 @@ public class gravtest implements Screen, InputProcessor {
 
     }
 }
+//http://www.gamefromscratch.com/post/2014/08/27/LibGDX-Tutorial-13-Physics-with-Box2D-Part-1-A-Basic-Physics-Simulations.aspx
